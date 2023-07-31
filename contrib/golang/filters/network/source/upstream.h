f241e03c5ffa8672ddcd8f07d84ffb04a15d305c
antJack <52443884+antJack@users.noreply.github.com>
2023-07-31 18:06:35 +0800

    Fix: flaky golang-network sandbox (#28687)
    Signed-off-by: yongjie.yyj <yeyeyongjie@gmail.com>

--- contrib/golang/filters/network/source/upstream.h
+++ contrib/golang/filters/network/source/upstream.h
@@ -36,7 +36,7 @@ class UpstreamConn : public Tcp::ConnectionPool::Callbacks,
                      Logger::Loggable<Logger::Id::golang> {
 public:
   UpstreamConn(std::string addr, Dso::NetworkFilterDsoPtr dynamic_lib,
-               Event::Dispatcher* dispatcher = nullptr);
+               unsigned long long int goConnID, Event::Dispatcher* dispatcher = nullptr);
   ~UpstreamConn() override {
     if (handler_) {
       handler_->cancel(Tcp::ConnectionPool::CancelPolicy::Default);
@@ -95,6 +95,7 @@ private:
   }
 
   Dso::NetworkFilterDsoPtr dynamic_lib_{nullptr};
+  unsigned long long int goConnID_{0};
   UpstreamConnWrapper* wrapper_{nullptr};
   Event::Dispatcher* dispatcher_{nullptr};
   std::unique_ptr<StreamInfo::StreamInfo> stream_info_{nullptr};
