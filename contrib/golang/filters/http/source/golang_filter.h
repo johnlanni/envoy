ed629e7e1fbee8219307c91a7a73f9c39775dbaf
罗泽轩 <spacewanderlzx@gmail.com>
2024-11-01 12:13:15 +0800

    golang: provide method to refresh route cache (#36863)
    <!--
    !!!ATTENTION!!!
    
    If you are fixing *any* crash or *any* potential security issue, *do
    not*
    open a pull request in this repo. Please report the issue via emailing
    envoy-security@googlegroups.com where the issue will be triaged
    appropriately.
    Thank you in advance for helping to keep Envoy secure.
    
    !!!ATTENTION!!!
    
    For an explanation of how to fill out the fields, please see the
    relevant section
    in
    [PULL_REQUESTS.md](https://github.com/envoyproxy/envoy/blob/main/PULL_REQUESTS.md)
    -->
    
    Commit Message: golang: provide method to refresh route cache
    Additional Description:
    Risk Level: Low, add new method
    Testing: Integration
    Docs Changes:
    Release Notes:
    Platform Specific Features:
    [Optional Runtime guard:]
    Fixes #36848
    [Optional Fixes commit #PR or SHA]
    [Optional Deprecated:]
    [Optional [API
    Considerations](https://github.com/envoyproxy/envoy/blob/main/api/review_checklist.md):]
    
    ---------
    
    Signed-off-by: spacewander <spacewanderlzx@gmail.com>

--- contrib/golang/filters/http/source/golang_filter.h
+++ contrib/golang/filters/http/source/golang_filter.h
@@ -259,7 +259,8 @@ public:
            const StreamInfo::StreamInfo& stream_info,
            Envoy::AccessLog::AccessLogType access_log_type) override;
 
-  CAPIStatus clearRouteCache();
+  CAPIStatus clearRouteCache(bool refresh);
+  void clearRouteCacheInternal(bool refresh);
   CAPIStatus continueStatus(ProcessorState& state, GolangStatus status);
 
   CAPIStatus sendLocalReply(ProcessorState& state, Http::Code response_code, std::string body_text,
